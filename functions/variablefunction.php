8e88fbf3dcac5eba5b4f484bf579e4040bb9d9f4
Mohib khan mahsud <f20bseen1m01107@iub.edu.pk>
2024-10-02 15:41:32 +0500

    dd

---
+++ functions/variablefunction.php
@@ -0,0 +1,19 @@
+<?php
+//var function is same as js we already cover either we stored the in var during the function exe or assign whole function in start to the variable 
+
+
+$fnc = function (){
+  echo 'its anonymous function ';
+};
+
+$fnc();
+
+
+function vrb(){
+  echo 'anotehr var function ';
+}
+
+$vrb = 'vrb';
+$vrb();
+
+?>
