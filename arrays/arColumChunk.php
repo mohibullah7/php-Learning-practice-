27ff349fe033820d7bcd65dbf4569bfb13ff789a
Mohib khan mahsud <f20bseen1m01107@iub.edu.pk>
2024-10-09 17:04:11 +0500

    today

--- arrays/arColumChunk.php
+++ arrays/arColumChunk.php
@@ -21,4 +21,26 @@ echo "<pre>";
 print_r($ar1);
 echo "</pre>";
 
+
+//array  chunk is used to make multidemsinal array from othr array it accept two paraeters first one is the array name and other one is the number how mnay pair of array we want to make it make multi dim associative array
+
+$b3=['one','teow','hqwuh',3232,232,3232,232,232];
+$chk =array_chunk($b3,2);
+echo "<pre>";
+print_r($chk);
+echo "</pre>";
+
+//the third parameter is the preserved value which by defualt false and we can set it true to get the key of the array 
+
+$r= [
+  'q'=>'we','w'=>'re',22=>'444',33=>'99'
+];
+$t = array_chunk($r,2,true);
+
+echo "<pre>";
+print_r($t);
+echo "</pre>";
+
+
+
 ?>
